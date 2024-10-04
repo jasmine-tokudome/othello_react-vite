6c00ed2b24c8e8c7ec1c16cd0af11b6cebdfd0fc
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-10-04 20:50:03 +0900

    石を置ける場所を判定する関数検討中

--- src/Game.tsx
+++ src/Game.tsx
@@ -77,6 +77,27 @@ function handleClick(row, col) {
     }
     setBlackIsNext(!blackIsNext)
 }
+checkStone(yIndex, xIndex, player, board = this.board) {
+
+  //チェックする方向
+  const directions = [
+  [0, 1], // 右
+  [0, -1], // 左
+  [-1, 0], // 上
+  [1, 0], // 下
+  [-1, -1], // 左上
+  [1, 1], // 左下
+  [-1, 1], // 右上
+  [1, -1], // 右下
+  ];
+  
+  const change = [];
+  directions.forEach((el) => {
+  const result = crossCheck(board,  { yIndex, xIndex }, el[0], el[1], player);
+  change.push(...result);
+  });
+  return change;
+  }
 
   return (
     <>
