26026ea7fbe99b6c613bfec0c2c5d4ea738b6e59
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-10-30 21:34:41 +0900

    checkStoneを見直していく

--- src/Game.tsx
+++ src/Game.tsx
@@ -5,28 +5,6 @@ import { crossCheck } from "./crossCheck"
 function Game() {
   const [count, setCount] = useState(0)
 
-  const checkStone = (rowIndex: number, colIndex: number, stoneColor: string, currentSquares = squares) => {
-
-    //チェックする方向
-    const directions = [
-    [0, 1], // 右
-    [0, -1], // 左
-    [-1, 0], // 上
-    [1, 0], // 下
-    [-1, -1], // 左上
-    [1, 1], // 左下
-    [-1, 1], // 右上
-    [1, -1], // 右下
-    ];
-    
-    const change = [];
-    directions.forEach((el) => {
-    const result = crossCheck(currentSquares,  { rowIndex, colIndex }, el[0], el[1], stoneColor);
-    change.push(...result);
-    });
-    return change;
-    }
-
   return (
     <>
       <div>
@@ -35,18 +13,6 @@ function Game() {
     </div>
     </>
   )
-}
-
-function Square({value, onSquareClick}) {
-
-  return (
-  <button
-    className="square"
-    onClick={onSquareClick}
-    >
-      {value}
-    </button>
-  );
 }
 
 function Board() {
@@ -64,6 +30,18 @@ function Board() {
   const [squares, setSquares] = useState(initialSquares);
 
 console.log(squares);
+
+function Square({value, onSquareClick}) {
+
+  return (
+  <button
+    className="square"
+    onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
 
 function handleClick(row: number, col: number) {
   const nextSquares = squares.map((r) => r.slice()); // 2次元配列をコピー
@@ -95,5 +73,29 @@ function handleClick(row: number, col: number) {
     </>
   );
  }
+
+// 以下見直していく
+//  const checkStone = (rowIndex: number, colIndex: number, stoneColor: string, currentSquares = squares) => {
+
+//   //チェックする方向
+//   const directions = [
+//   [0, 1], // 右
+//   [0, -1], // 左
+//   [-1, 0], // 上
+//   [1, 0], // 下
+//   [-1, -1], // 左上
+//   [1, 1], // 左下
+//   [-1, 1], // 右上
+//   [1, -1], // 右下
+//   ];
+  
+//   const change = [];
+//   directions.forEach((el) => {
+//   const result = crossCheck(currentSquares,  { rowIndex, colIndex }, el[0], el[1], stoneColor);
+//   change.push(...result);
+//   });
+
+//   return change;
+//   }
 
 export default Game
